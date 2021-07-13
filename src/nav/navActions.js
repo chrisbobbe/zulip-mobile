17cc64f8ad600e213a0474b5e8c69817bed8abbb
Chris Bobbe <cbobbe@zulip.com>
2021-07-12 20:12:38 -0400

    navActions types: Tighten navigateBack's return type a bit.

--- src/nav/navActions.js
+++ src/nav/navActions.js
@@ -1,5 +1,6 @@
 /* @flow strict-local */
 import {
+  type PopAction,
   StackActions,
   CommonActions,
   type GenericNavigationAction,
@@ -9,7 +10,7 @@ import type { Message, Narrow, SharedData, UserId } from '../types';
 import type { ApiResponseServerSettings } from '../api/settings/getServerSettings';
 import { getSameRoutesCount } from '../selectors';
 
-export const navigateBack = (): GenericNavigationAction => StackActions.pop(getSameRoutesCount());
+export const navigateBack = (): PopAction => StackActions.pop(getSameRoutesCount());
 
 /*
  * "Reset" actions, to explicitly prohibit back navigation.
